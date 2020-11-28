b7a5c124c6c6b8d24b21454ecf46f14a5d71dcf5
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-11-28 17:26:45 +0100

    getting jwt idtoken value

--- src/main/java/com/appsdeveloperblog/ws/clients/controller/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/controller/AlbumsController.java
@@ -1,7 +1,13 @@
 package com.appsdeveloperblog.ws.clients.controller;
 
 import com.appsdeveloperblog.ws.clients.response.AlbumRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -14,9 +20,20 @@ import java.util.List;
 @Controller
 public class AlbumsController {
 
+    @Autowired
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal){
 
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken oAuth2Token = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(
+                oAuth2Token.getAuthorizedClientRegistrationId(),
+                oAuth2Token.getName());
+
+        System.out.println("JWT : "+ client.getAccessToken().getTokenValue());
 
         System.out.println("Principal "+principal);
         OidcIdToken idToken=principal.getIdToken();
